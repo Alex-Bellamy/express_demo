88aed912aa9ad59f5c524791f70f1c199de4a3c9
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-25 23:23:32 +0200

    initial commit/demo

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.log(req.query)
+  let greeting = req.query.message || "World"
+
+  res.send(`<h1>Hello ${greeting}</h1>`)
+})
+
+app.listen(port, () => {
+  console.warn(`The audience is listening on port ${port}`)
+})
